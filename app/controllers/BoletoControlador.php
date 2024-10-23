4ac61250023f0b783d7003e5448226a1fdff34d8
Francisco Cocirio <fcocirio@gmail.com>
2024-10-23 18:59:29 -0300

    db

--- app/controllers/BoletoControlador.php
+++ app/controllers/BoletoControlador.php
@@ -64,11 +64,13 @@ class BoletoControlador{
             !isset($req->body->precio) || empty($req->body->precio)){
             return $this->vista->response('Faltan completar datos', 400);
         }
-
+        if(is_int($req->body->precio)){
+            $precio = $req->body->precio;
+        }
         $destino_inicio = $req->body->destino_inicio;       
         $destino_fin = $req->body->destino_fin;       
         $fecha_salida = $req->body->fecha_salida;
-        $precio = $req->body->precio;
+        
 
         $this->modelo->editarBoleto($id, $destino_inicio, $destino_fin, $fecha_salida, $precio);
 
